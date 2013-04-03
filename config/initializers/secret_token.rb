591e6cb1d25f25c2d55284e640d260776b44823d
Web Design <WebDesign@News-iMac-60.local>
2013-04-02 20:09:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkyourteeth::Application.config.secret_token = '5394c8d5d8a5e5aeaca9c0abb8bb31fb025620aca4e3b7776166b2582f816f0ab00bf76ead99417cd40675ec91502200ca77b727bb3e94fe77d87cbf2e46a667'
